6873db2cc24a1acf52fac8aa064e8344e1a8a56e
Sijan Adhikari <adhikarisijan909@gmail.com>
2024-06-22 00:05:49 +0545

    Make add to cart button interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -46,7 +46,7 @@ products.forEach((product)=>{
                     Added
                 </div>
 
-                <button class="add-to-cart-button button-primary">
+                <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">  
                     Add to Cart
                 </button>
                 </div>
@@ -58,3 +58,40 @@ products.forEach((product)=>{
 console.log(productsHTML);
 
 document.querySelector('.js-products-grid').innerHTML=productsHTML;
+
+
+document.querySelectorAll('.js-add-to-cart')
+    .forEach((button)=>{
+        button.addEventListener('click',()=>{
+        const productId=  button.dataset.productId;
+
+        let matchingItem;
+        cart.forEach((item)=>{
+            if(productId=== item.productId){
+                matchingItem= item;
+            }
+        });
+
+        if(matchingItem){
+            matchingItem.quanitiy+=1;
+        } else{
+            cart.push({
+                productId:productId,
+                quanitiy: 1
+    
+            });
+        }
+        console.log(cart); 
+        });
+    });
+
+
+    /*
+    in line 49 
+
+    How do we know which product to add to cart?
+
+    Data Attribute:
+    - is just another HTML attribute
+    - allows us to attach any information to an element
+    */
