1441b51bd7b4764e49826473095b7b65cef56ec9
Sijan Adhikari <adhikarisijan909@gmail.com>
2024-07-03 23:44:49 +0545

    delete products from HTML when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -14,7 +14,7 @@ cart.forEach((cartItem)=>{
         }
     });
 cartSummaryHTML+=`
- <div class="cart-item-container">
+ <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
               Delivery date: Wednesday, June 15
             </div>
@@ -100,5 +100,11 @@ document.querySelector('.js-order-summary')
        const productId= link.dataset.productId;
        removeFromCart(productId);
        
+
+       const container=document.querySelector(
+        `.js-cart-item-container-${productId}`
+
+       );
+       container.remove();
       });
     });
